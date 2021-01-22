60f7b302d47a353c8cecbd5ee00d607eeaf74737
Borhan Uddin Bhuiyan <borhanuddinbhuiyan303@gmail.com>
2021-01-22 23:52:23 +0600

    1st programming sum of two integer number

---
+++ Algorithmic Toolbox/Week 1/APlusB.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+
+// where the execution of program begins 
+int main() 
+{ 
+	int a=0, b=0, sum=0;
+  std::cin>>a;
+  std::cin>>b;
+  std::cout<<a+b;
+	
+	return 0; 
+} 
